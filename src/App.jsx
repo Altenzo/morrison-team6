a81c97428508242f92179449d1f1f191cffb0c50
lakslai <t045433@gmail.com>
2021-08-21 18:48:29 +0600

    chore: Елдар посоветовал добавить компонент, и мы его затестим

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,24 @@
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-import imge from "./imge/logo.png";
 import "./styleM.css";
-import img from "./imge/icon.png";
-import whatsapp from "./imge/whatsapp.png";
-import vk from "./imge/vk.png";
-import insta from "./imge/insta.png";
-import welcom from "./imge/welcom.jpg";
+
 import GameList from "./API.jsx";
 import GameItem from "./GameItem";
+import Header from "./Header";
+import Welcom from "./companent/Welcom.jsx";
 
 function App() {
   return (
     <div className="Mani">
       <div className="container">
         <BrowserRouter>
-          <header>
-            <div className="headerList">
-              <div className="logo">
-                <img src={imge} alt="" />
-              </div>
-
-              <div className="headerText">
-                <div className="glav">
-                  <Link>
-                    <p>Главная</p>
-                  </Link>
-                </div>
-
-                <div className="navigater">
-                  <Link>
-                    <p>Категорий </p>
-                    <img src={img} />
-                  </Link>
-                </div>
-
-                <div className="navigatertwo">
-                  <Link>
-                    <p>Платформы</p>
-                    <img src={img} />
-                  </Link>
-                </div>
-              </div>
-
-              <div className="logIn">
-                <Link to="LogIn">
-                  <button className="buttonLogin">Логин</button>
-                </Link>
-              </div>
-
-              <div class="mesenger">
-                <div class=" whatsapp">
-                  <img src={whatsapp} alt="" />
-                </div>
-
-                <div class=" vk">
-                  <img src={vk} alt="" />
-                </div>
-
-                <div class="insta">
-                  <img src={insta} alt="" />
-                </div>
-              </div>
-            </div>
-          </header>
-
-          <section className="welcom">
-            <div className="WelcomList">
-              <div className="WelcomImge">
-                <img src={welcom} alt="" />
-              </div>
-              <div className="WelcomText">
-                <span>
-                  <h2>
-                    Найдите и скачайте свою любимую <br />{" "}
-                    <a href="">БЕСПЛАТНУЮ</a> игру
-                  </h2>
-                </span>
-              </div>
-              <div className="WelcomButton">
-                <button className="butoncotolog">Начать</button>
-                <button className="buttonserh">Наити</button>
-              </div>
-            </div>
-          </section>
-
-          {/* <Switch>
-            <Route>
-              <GameItem />
-            </Route>
-          </Switch>  */}
+          <Switch>
+         <Header />
+          <Welcom />
+          </Switch> 
         </BrowserRouter>
       </div>
     </div>
-  );
+  );  
 }
 
 export default App;
---
+++ src/companent/Welcom.jsx
@@ -0,0 +1,27 @@
+import welcom from "../imge/welcom.jpg";
+
+function Welcom() {
+  return (
+    <section className="welcom">
+      <div className="WelcomList">
+        <div className="WelcomImge">
+          <img src={welcom} alt="" />
+        </div>
+        <div className="WelcomText">
+          <span>
+            <h2>
+              Найдите и скачайте свою любимую <br /> <a href="">БЕСПЛАТНУЮ</a>{" "}
+              игру
+            </h2>
+          </span>
+        </div>
+        <div className="WelcomButton">
+          <button className="butoncotolog">Начать</button>
+          <button className="buttonserh">Наити</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Welcom;
