9872ada7d83dd6723cbe3734fcd3bbf6b7cbcb18
kiborg123 <sydyk03@list.ru>
2021-08-21 19:43:11 +0600

    полнастью закончил хэдер

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import "./styleM.css";
-<<<<<<< HEAD:src/App.js
 import img from "./imge/icon.png";
 import whatsapp from "./imge/whatsapp.png";
 import vk from "./imge/vk.png";
 import insta from "./imge/insta.png";
 import welcom from "./imge/welcom.jpg";
 import GameList from "./API.js";
-=======
-
-import GameList from "./API.jsx";
->>>>>>> 3c2887acfc9841d7f485af4a85d3b5bb4680f1d9:src/App.jsx
 import GameItem from "./GameItem";
 import Header from "./Header";
 import Welcom from "./companent/Welcom.jsx";
---
+++ src/Header.js
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import icon from "./image/icon.png";
+import whatsapp from "./image/whatsapp.png";
+import vk from "./image/vk.png";
+import logo from "./image/logo.png";
+import insta from "./image/insta.png";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="bacgr">
+
+        <div className="header-inner">
+          <div className="header-container">
+            <div className="logo">
+              <img src={logo} alt="" />
+            </div>
+
+            <nav className="nav">
+              <div className="nav-container">
+                <ul className="ul">
+                  <li>
+                    <a href="">Главная</a> <img className="icon" src={icon} alt="" />
+                  </li>
+                  <li>
+                    <a href="">Категории</a> <img className="icon" src={icon} alt="" />
+                  </li>
+                  <li>
+                    <a href="">Платформы</a> <img className="icon" src={icon} alt="" />
+                  </li>
+                </ul>
+              </div>
+            </nav>
+
+            <div className="sign-in">
+              <button className="sign-in_btn">LOGIN</button>
+
+              <div className="image-sign-in">
+                <img src={whatsapp} alt="" />
+                <img src={insta} alt="" />
+                <img src={vk} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+export default Header;
