4aa1e399d1b4ca025345712ae91fea0aaed7e7cf
kiborg123 <sydyk03@list.ru>
2021-08-21 20:10:21 +0600

    header

--- src/API.js
+++ src/API.js
@@ -4,7 +4,6 @@
 // function GameList () {
 // 	const[game, setGamelist] = useState([])
 
-<<<<<<< HEAD
 // 	useEffect(()=> {
 // 		fetch('https://free-to-play-games-database.p.rapidapi.com/api/filter?tag=3d.mmorpg.fantasy.pvp&platform=pc', 
 // 	"method": "GET",
@@ -17,20 +16,6 @@
 // 	response.json()
 // )
 // .then(data => console.log(data))
-=======
-	useEffect(()=> {
-		fetch('https://free-to-play-games-database.p.rapidapi.com/api/filter?tag=3d.mmorpg.fantasy.pvp&platform=pc', 
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
-		"x-rapidapi-key": "a2527e14b3msh8054ffeba10e8eep144ae1jsn286db16d9c5e"
-	}
-},[])
-.then(response => 
-	response.json()
-)
-.then(data => console.log(data))
->>>>>>> 4b50535311f207d21094bc5a09e98ef944a59893
 
 // })
 
@@ -44,8 +29,4 @@
 // 	)
 // }
 
-<<<<<<< HEAD
 // export default GameList
-=======
-export default GameList
->>>>>>> 4b50535311f207d21094bc5a09e98ef944a59893
