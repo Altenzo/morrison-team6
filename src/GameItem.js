4aa198214c67f7b96608002b185215289a7b1d5f
kiborg123 <sydyk03@list.ru>
2021-08-24 18:34:46 +0600

    marat

--- src/GameItem.js
+++ src/GameItem.js
@@ -6,8 +6,13 @@ function GameItem(props) {
     <Link to={"/info/" + props.data.id} className="GameItem">
       <div className="cony">
         <div className="container-api">
+          <div className="img-api">
             <img src={props.data.thumbnail} alt="" />
+          </div>
+
+          <div className="title-api">
             <h3>{props.data.title}</h3>
+          </div>
         </div>
       </div>
 
