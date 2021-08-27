64b2ccf3391706c444238aa76fa68cdf8464b3ba
kiborg123 <sydyk03@list.ru>
2021-08-27 14:00:08 +0600

    marat

--- src/GameItem.js
+++ src/GameItem.js
@@ -6,7 +6,6 @@ function GameItem(props) {
     <Link to={"/info/" + props.data.id} className="GameItem">
       <div className="cony">
         <div className="container-api">
-<<<<<<< HEAD
           <div className="img-api">
             <img src={props.data.thumbnail} alt="" />
           </div>
@@ -14,14 +13,6 @@ function GameItem(props) {
           <div className="title-api">
             <h3>{props.data.title}</h3>
           </div>
-=======
-          <img
-            src={props.data.thumbnail}
-            alt=""
-            className="container-api-img"
-          />
-          <h3>{props.data.title}</h3>
->>>>>>> 9fbbd09bbddb43394b8c3fc59fd5dde8ecb4820d
         </div>
       </div>
     </Link>
--- src/ID/info.jsx
+++ src/ID/info.jsx
@@ -29,20 +29,46 @@ function Info() {
         setInfo(game.filter((el) => String(el.id) == String(id))[0])
     }, [id])
 
-    if (!info) return ('ничего нет')
 
 
-    return (
-        <div className="info-page">
-            <img src={info.thumbnail} alt="" />
-            <h1>{info.title}</h1>
-            <h1>{info.release_date}</h1>
-            <h1>{info.genre}</h1>
-            <h1>{info.short_description}</h1>
-            <a href="">{info.freetogame_profile_url}</a>
-        </div>
-    )
+    if (info) {
+        return (
+            <div className="info-page">
+                <div className="info-inner">
+                    <div className="info-container">
+                        <div className="info-df">
+                            <div className="info-img">
+                                <img src={info.thumbnail} alt="" />
+                                <button>ИГРАТЬ</button>
+                            </div>
 
+
+                            <div className="info-title">
+                                <div className="info-h1">
+
+                                    <h1>{info.title}</h1>
+                                </div>
+                                <div className="info-desc">
+                                    <h1>{info.short_description}</h1>
+                                    <h1>{info.release_date}</h1>
+                                    <h1>{info.genre}</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="info-info">
+                        <div className="title-info2">
+                            <h1>Краткая информация</h1>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        )
+    } else {
+        return ("не найдено")
+    }
 }
 
 export default Info
